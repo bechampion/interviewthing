81f66e718083f1661e1d73e1d794f9465bab0a00
jgarcia <garciaj.uk@gmail.com>
2018-10-26 17:59:05 +0100

    vpc instance-tenancy does not need to be dedicated

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,6 @@ module "kube-vpc" {
   vpc_cidr_block = "10.0.0.0/16"
   tags = "${map("Name","kube-vpc","Reason","Kubernetes Cluster","Environment","interview")}"
   private_subnets = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  public_subnets = ["10.0.128.0/24","10.0.129.0./24","10.0.130.0/24"]
+  public_subnets = ["10.0.128.0/24","10.0.129.0/24","10.0.130.0/24"]
 
 }
--- infra/modules/aws/vpc/vpc.tf
+++ infra/modules/aws/vpc/vpc.tf
@@ -1,5 +1,4 @@
 resource "aws_vpc" "kube-vpc" {
   cidr_block       = "${var.vpc_cidr_block}"
-  instance_tenancy = "dedicated"
   tags = "${var.tags}"
 }
