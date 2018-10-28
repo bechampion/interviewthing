9fe95cd3e48536ba7825d1ee83b431df1d90021f
jgarcia <garciaj.uk@gmail.com>
2018-10-28 17:35:33 +0000

    fixing type on subnets per AZ

--- infra/modules/aws/vpc/02_subnets.tf
+++ infra/modules/aws/vpc/02_subnets.tf
@@ -7,12 +7,12 @@ resource "aws_subnet" "vpc-public-subnet" {
   vpc_id     = "${aws_vpc.kube-vpc.id}"
   cidr_block = "${element(var.public_subnets,count.index)}"
   tags = "${merge(map("kubernetes.io/cluster/${var.cluster_name}", "shared","Name","vpc-public-subnet-${count.index}"),var.tags)}"
-  availability_zone = "eu-west-1${local.az[count.index}}"
+  availability_zone = "eu-west-1${local.az[count.index]}"
 }
 resource "aws_subnet" "vpc-private-subnet" {
   count = "${length(var.private_subnets)}"
   vpc_id     = "${aws_vpc.kube-vpc.id}"
   cidr_block = "${element(var.private_subnets,count.index)}"
   tags = "${merge(map("kubernetes.io/cluster/${var.cluster_name}", "shared","Name","vpc-private-subnet-${count.index}"),var.tags)}"
-  availability_zone = "eu-west-1${local.az[count.index}}"
+  availability_zone = "eu-west-1${local.az[count.index]}"
 }
